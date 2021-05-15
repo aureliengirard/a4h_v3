97747c3a0220aea97eab72cb49fdb747b2eba38a
Aurelien Girard <contact@aureliengirard.com>
2021-05-15 16:30:54 +0300

    commit all un commited content

--- assets/products.js
+++ assets/products.js
@@ -1,5 +1,10 @@
+var ungoal_select = document.getElementById("ungoal-select");
+var buy_button = document.getElementById("buy-button");
+var alert = document.getElementById("buy-button__alert");
+
 const thumbnails = document.getElementsByClassName('preview-thumbnail');
 const previewImg = document.getElementById('product-image');
+
 for (var i = 0, len = thumbnails.length; i < len; i++) {
     const currentThumb = thumbnails[i];
     currentThumb.addEventListener('click', (e) => {
@@ -26,14 +31,30 @@ $(document).ready(function() {
     }
 });
 
-
 function ungoalSelection() {
 
-    var selected_un_goal = document.getElementById("ungoal-select").value;
+    var selected_un_goal = ungoal_select.value;
     var product_title = document.getElementById("product-title").value;
 
+    if( $('#ungoal-select').val() === "" ) {
+        buy_button.classList.add('disabled');
+        ungoal_select.classList.remove('selected');
+    } else {
+        ungoal_select.classList.add('selected');
+        buy_button.classList.remove('disabled');
 
-    $("#buy-button").prop('disabled', false);
+        if ($('#buy-button__alert').hasClass( "is_active" )) {
+            alert.classList.remove('is_active');
+        }
 
-    window.sessionStorage.setItem(product_title, selected_un_goal);
+        window.sessionStorage.setItem(product_title, selected_un_goal);
+    }
+
 };
+
+$("#buy-button").on("click", function(e) {
+    if ($( this ).hasClass( "disabled" )){
+        e.preventDefault();
+        alert.classList.add('is_active');
+    }
+});
